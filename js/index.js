1f95f248fb25cc00f4728b64257fc195f1c7009a
Alina Basiuk <basyuk98@gmail.com>
2021-06-12 14:25:12 -0400

    updated loader
    updated loader

--- js/index.js
+++ js/index.js
@@ -1,40 +1,13 @@
-<<<<<<< Updated upstream
-$('.page').each(function(i,e){
-    $(this).click(function(event){
-      var x = event.pageX;
-      var y = event.pageY;
-      
-      var nextItem = i + 1;
-      if (nextItem >= $('.page').length){
-        nextItem = 0;
-      }
-      
-      $('.page:eq('+ nextItem +')').css('z-index', parseInt($(this).css('z-index')) + 1);
-      $('.page:eq('+ nextItem +')').css('clip-path', 'circle(0% at '+ x +'px '+ y +'px)');
-      
-      anime({
-        targets: $('.page')[nextItem],
-        update: function(anim) {
-          $('.page:eq('+ nextItem +')').css('clip-path', 'circle('+ (anim.progress*2) +'% at '+ x +'px '+ y +'px)');
-        }
-      });
-    });
-  });
+$(document).ready(function () {
+    $('#fadeIn').hide()
+    console.log("I am loaded")
 
-=======
-function fade() {
-    $('.preloader').fadeOut("fast");
-    }
-    setTimeout(fade, 1000);
+    setTimeout(() => {
+        $('#fadeIn').show()
 
-    $(document).ready(function () {
-        $('#nav-links li').hover(
-        function () {
-            //show submenu
-            $('ul', this).slideDown("fast");
-        }, function () {
-            //hide submenu
-            $('ul', this).slideUp("fast");
-        });
-    });
->>>>>>> Stashed changes
+
+ 
+
+        $("#preLoader").hide()
+    }, 1500)
+});
