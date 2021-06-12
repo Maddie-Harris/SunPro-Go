789e257ff545222a88483d6a86bf2e38808fb182
Alina Basiuk <basyuk98@gmail.com>
2021-06-12 11:03:40 -0400

    Preloader
    added preloader

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 $('.page').each(function(i,e){
     $(this).click(function(event){
       var x = event.pageX;
@@ -20,3 +21,20 @@ $('.page').each(function(i,e){
     });
   });
 
+=======
+function fade() {
+    $('.preloader').fadeOut("fast");
+    }
+    setTimeout(fade, 1000);
+
+    $(document).ready(function () {
+        $('#nav-links li').hover(
+        function () {
+            //show submenu
+            $('ul', this).slideDown("fast");
+        }, function () {
+            //hide submenu
+            $('ul', this).slideUp("fast");
+        });
+    });
+>>>>>>> Stashed changes
